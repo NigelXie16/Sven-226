8e579586f6bfb7c38656cad310bbb70d7770d57b
Nigel Xie <nx1027@bard.edu>
2025-02-12 23:27:46 -0500

    Added Comments

--- 226/labs/HW2/nx1027h2/ccount.c
+++ 226/labs/HW2/nx1027h2/ccount.c
@@ -1,21 +1,28 @@
+// Nigel Xie
+//nx1027
+//02/12/2025
 #include <stdio.h>
 
+/*
+ * This program counts the frequency of each letter in the input.
+ */
 int main() {
-	char ch;
-	int list[128] = {0};
+	char ch; // character to be read or is being read
+	int list[128] = {0}; // array to store the frequency of each letter
+	// Using ASCII values as the index of the array
 
-	while ((ch = getchar()) != EOF) {
-		if (ch >= 'A' && ch <= 'Z') {
-			list[(int)ch + 32]++;
+	while ((ch = getchar()) != EOF) { // read characters until EOF, end of file
+		if (ch >= 'A' && ch <= 'Z') { // if the character is uppercase
+			list[(int)ch + 32]++; // convert to lowercase and increment frequency
 		}
-		else if (ch >= 'a' && ch <= 'z') {
-			list[(int)ch]++;
+		else if (ch >= 'a' && ch <= 'z') { // if the character is lowercase
+			list[(int)ch]++; // increment frequency
 		}
 	}
 
 	int index;
-	for (index = 97; index <= 122; index++) {
-		printf("%c\t%i\n", index, list[index]);
+	for (index = 97; index <= 122; index++) { // loop through the lowercase letters
+		printf("%c\t%i\n", index, list[index]); // print the letter and its frequency
 	}
 	return 0;
 }
